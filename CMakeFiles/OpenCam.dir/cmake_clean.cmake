b1b43dbfa9b852c1806e2b0123ec6e0c5792e39f
kvzhao <kevinzhaio@gmail.com>
2013-10-14 17:34:05 +0800

    First import file

---
+++ CMakeFiles/OpenCam.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+FILE(REMOVE_RECURSE
+  "CMakeFiles/OpenCam.dir/opencam.c.o"
+  "OpenCam.pdb"
+  "OpenCam"
+)
+
+# Per-language clean rules from dependency scanning.
+FOREACH(lang C)
+  INCLUDE(CMakeFiles/OpenCam.dir/cmake_clean_${lang}.cmake OPTIONAL)
+ENDFOREACH(lang)
